cc8e89be78bb81802d4bb43b2ef9bd2194ef92ff
Manuel Pfemeter <manuel.pfemeter@gmx.de>
2014-08-29 19:00:01 +0200

    added more tests for metadata validation

--- src/Woff.net.Tests/WoffHeaderValidatorTests.cs
+++ src/Woff.net.Tests/WoffHeaderValidatorTests.cs
@@ -23,5 +23,50 @@ namespace WoffDotNet.Tests
             // assert
             actualResult.Should().BeTrue();
         }
+
+        [Fact]
+        public void HasIllegalMetadata_Should_BeTrue_BecauseItHasNoOffsetAndALength()
+        {
+            // arrange
+            uint offset = 0;
+            uint length = 200;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, offset, length, 0, 0, 0);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalMetadata(header);
+
+            // assert
+            actualResult.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HasIllegalMetadata_Should_BeFalse_BecauseItHasAnOffsetAndALength()
+        {
+            // arrange
+            uint offset = 200;
+            uint length = 200;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, offset, length, 0, 0, 0);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalMetadata(header);
+
+            // assert
+            actualResult.Should().BeFalse();
+        }
+
+        [Fact]
+        public void HasIllegalMetadata_Should_BeFalse_BecauseItHasNoOffsetAndNoLength()
+        {
+            // arrange
+            uint offset = 0;
+            uint length = 0;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, offset, length, 0, 0, 0);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalMetadata(header);
+
+            // assert
+            actualResult.Should().BeFalse();
+        }
     }
 }

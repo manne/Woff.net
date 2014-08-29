6d82047e23f6a74c15f01634612487ab0629bea5
Manuel Pfemeter <manuel.pfemeter@gmx.de>
2014-08-29 19:38:38 +0200

    added helper methods

---
+++ src/Woff.net/WoffHelpers.cs
@@ -0,0 +1,28 @@
+﻿using WoffDotNet.Types;
+using WoffDotNet.Validators;
+
+namespace WoffDotNet
+{
+    public static class WoffHelpers
+    {
+        public static bool HasMetadata(this WoffHeader header)
+        {
+            if (WoffHeaderValidator.HasIllegalMetadata(header))
+            {
+                return false;
+            }
+
+            return header.MetaOffset > 0;
+        }
+
+        public static bool HasPrivateDate(this WoffHeader header)
+        {
+            if (WoffHeaderValidator.HasIllegalPrivateData(header))
+            {
+                return false;
+            }
+
+            return header.PrivOffset > 0;
+        }
+    }
+}

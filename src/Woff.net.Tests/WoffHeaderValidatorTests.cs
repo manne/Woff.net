8dd48d0f22600b71eddc36f28150cdeed1ad2e09
Manuel Pfemeter <manuel.pfemeter@gmx.de>
2014-08-29 19:02:09 +0200

    added tests for validation of private data

--- src/Woff.net.Tests/WoffHeaderValidatorTests.cs
+++ src/Woff.net.Tests/WoffHeaderValidatorTests.cs
@@ -68,5 +68,65 @@ namespace WoffDotNet.Tests
             // assert
             actualResult.Should().BeFalse();
         }
+
+        [Fact]
+        public void HasIllegalPrivateData_Should_BeTrue_BecauseItHasAnOffsetAndNoLength()
+        {
+            // arrange
+            uint offset = 200;
+            uint length = 0;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, offset, length);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalPrivateData(header);
+
+            // assert
+            actualResult.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HasIllegalPrivateData_Should_BeTrue_BecauseItHasNoOffsetAndALength()
+        {
+            // arrange
+            uint offset = 0;
+            uint length = 200;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, offset, length);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalPrivateData(header);
+
+            // assert
+            actualResult.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HasIllegalPrivateData_Should_BeFalse_BecauseItHasAnOffsetAndALength()
+        {
+            // arrange
+            uint offset = 200;
+            uint length = 200;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, offset, length);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalPrivateData(header);
+
+            // assert
+            actualResult.Should().BeFalse();
+        }
+
+        [Fact]
+        public void HasIllegalPrivateData_Should_BeFalse_BecauseItHasNoOffsetAndNoLength()
+        {
+            // arrange
+            uint offset = 0;
+            uint length = 0;
+            var header = new WoffHeader(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, offset, length);
+
+            // act
+            var actualResult = WoffHeaderValidator.HasIllegalPrivateData(header);
+
+            // assert
+            actualResult.Should().BeFalse();
+        }
     }
 }

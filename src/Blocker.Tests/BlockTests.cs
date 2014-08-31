2404f5db30855e6aed52e7ef693f51c85ade0323
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2014-08-31 19:43:13 +0200

    added support for block checking

---
+++ src/Blocker.Tests/BlockTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+using Xunit.Extensions;
+
+namespace Blocker.Tests
+{
+    public class BlockTests
+    {
+        [Theory]
+        [InlineData((uint)0, (uint)2)]
+        [InlineData((uint)10, (uint)2)]
+        public void ShouldHaveTheCorrectStartAndEndFromStartAndDistance(uint start, uint distance)
+        {
+            // arrange
+            var expectedResult = new Block(start, start + distance);
+
+            // act
+            var actualResult = Block.CreateFromStartAndDistance(start, distance);
+
+            // assert
+            actualResult.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ src/Blocker/Block.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Blocker
+{
+    public class Block : IEquatable<Block>
+    {
+        public Block(uint start, uint end)
+        {
+            Contract.Requires(start < end);
+
+            Start = start;
+            End = end;
+        }
+
+        public static Block CreateFromStartAndDistance(uint start, uint distance)
+        {
+            Contract.Requires(distance > 0);
+            Contract.Ensures(Contract.Result<Block>() != null);
+
+            return new Block(start, start + distance);
+        }
+
+        public uint Start { get; private set; }
+
+        public uint End { get; private set; }
+
+        #region IEquatable
+
+        public bool Equals(Block other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Start == other.Start && End == other.End;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj is Block && Equals((Block)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ((int)Start * 397) ^ (int)End;
+        }
+
+        public static bool operator ==(Block left, Block right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Block left, Block right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format("Start: {0}, End: {1}", Start, End);
+        }
+    }
+}

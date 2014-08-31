e87629c901412e4dc8c81cc3d325ab6df9c56d66
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2014-08-31 19:46:30 +0200

    added block for nesting

---
+++ src/Blocker/NestedBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Blocker
+{
+    public class NestedBlock : Block
+    {
+        private readonly List<Block> _childs = new List<Block>(10);
+
+        public NestedBlock(uint start, uint end)
+            : base(start, end)
+        {
+            // empty
+        }
+
+        public ReadOnlyCollection<Block> Childs
+        {
+            get
+            {
+                return new ReadOnlyCollection<Block>(_childs);
+            }
+        }
+
+        public void AddChild(Block child)
+        {
+            _childs.Add(child);
+        }
+
+        public bool Validate()
+        {
+            return true;
+        }
+    }
+}

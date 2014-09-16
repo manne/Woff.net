b21fce66bfa0d7e5c88435b04d02b4c84fa7b6da
manne <-unset>
2014-09-16 19:26:56 +0200

    added documentation

--- src/Woff.net/Types/WoffTableDirectory.cs
+++ src/Woff.net/Types/WoffTableDirectory.cs
@@ -5,6 +5,9 @@ namespace WoffDotNet.Types
 {
     public class WoffTableDirectory
     {
+        /// <summary>
+        /// The size of a table directory.
+        /// </summary>
         public const uint Size = 20;
 
         public WoffTableDirectory(UInt32 tag, UInt32 offset, UInt32 compLength, UInt32 origLength, UInt32 origCheckSum)
@@ -16,24 +19,42 @@ namespace WoffDotNet.Types
             OrigCheckSum = origCheckSum;
         }
 
+        /// <summary>
+        /// Gets or sets the 4-byte sfnt table identifier.
+        /// </summary>
         [DebuggerDisplay("{TagAsString()}, {Tag}")]
         public uint Tag { get; set; }
 
+        /// <summary>
+        /// Gets or sets the offset to the data, from beginning of WOFF file.
+        /// </summary>
         public uint Offset { get; set; }
 
+        /// <summary>
+        /// Gets or sets the length of the compressed data, excluding padding.
+        /// </summary>
         public uint CompLength { get; set; }
 
+        /// <summary>
+        /// Gets or sets the length of the uncompressed table, excluding padding.
+        /// </summary>
         public uint OrigLength { get; set; }
 
+        /// <summary>
+        /// Gets or sets the checksum of the uncompressed table.
+        /// </summary>
         public uint OrigCheckSum { get; set; }
 
+        /// <summary>
+        /// Gets or sets the padding to the next block.
+        /// </summary>
         internal uint Padding { get; set; }
 
         /// <summary>
         /// Returns the tag as a printable string.
         /// </summary>
         /// <returns>The tag as a printable string.</returns>
-        public String TagAsString()
+        public string TagAsString()
         {
             return new string(Tag.UInt32BEToCharArray());
         }

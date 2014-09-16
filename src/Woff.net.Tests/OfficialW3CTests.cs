5f7d6ede818eff4f2e9e80026c71c92fb8729e25
manne <-unset>
2014-09-16 20:43:49 +0200

    added official test blocks-extraneous-data-007

--- src/Woff.net.Tests/OfficialW3CTests.cs
+++ src/Woff.net.Tests/OfficialW3CTests.cs
@@ -605,6 +605,19 @@ namespace WoffDotNet.Tests
         }
 
         [Fact]
+        public void Invalid_Blocks_Extraneous_Data_007()
+        {
+            // arrange
+            var cut = GetReader(Resources.blocks_extraneous_data_007);
+
+            // act
+            Action action = cut.Process;
+
+            // assert
+            action.ShouldThrow<AggregateException>().And.InnerExceptions.Should().Contain(e => e.GetType() == typeof(BlockMaxPaddingExceededException));
+        }
+
+        [Fact]
         public void Invalid_Blocks_Overlap_001()
         {
             // arrange
--- src/Woff.net.Tests/Properties/Resources.Designer.cs
+++ src/Woff.net.Tests/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace WoffDotNet.Tests.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        internal static byte[] blocks_extraneous_data_007 {
+            get {
+                object obj = ResourceManager.GetObject("blocks_extraneous_data_007", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         internal static byte[] blocks_metadata_absent_001 {
             get {
                 object obj = ResourceManager.GetObject("blocks_metadata_absent_001", resourceCulture);

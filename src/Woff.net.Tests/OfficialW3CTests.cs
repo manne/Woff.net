fb5e121f234d7e9bf6b5ca73d9e7332383775df5
manne <-unset>
2014-09-17 19:00:16 +0200

    added official test metadata-well-formed-004

--- src/Woff.net.Tests/OfficialW3CTests.cs
+++ src/Woff.net.Tests/OfficialW3CTests.cs
@@ -255,6 +255,32 @@ namespace WoffDotNet.Tests
         }
 
         [Fact]
+        public void Invalid_Metadata_Well_Formed_004_CorrectException()
+        {
+            // arrange
+            var cut = GetReader(Resources.metadata_well_formed_004);
+
+            // act
+            cut.Process();
+
+            // assert
+            cut.MetadataExceptions.Should().ContainItemsAssignableTo<XmlException>();
+        }
+
+        [Fact]
+        public void Invalid_Metadata_Well_Formed_004_NoMetadata()
+        {
+            // arrange
+            var cut = GetReader(Resources.metadata_well_formed_004);
+
+            // act
+            cut.Process();
+
+            // assert
+            cut.Metadata.Should().BeNull();
+        }
+
+        [Fact]
         public void Invalid_Metadata_MetaOrigLength_002_CorrectException()
         {
             // arrange
